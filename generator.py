8a06a2b040b2f2e7e708b343fb51715dd3bea42d
Martha Kelly <mk.girdler@gmail.com>
2012-07-17 13:54:19 -0400

    meta working

--- generator.py
+++ generator.py
@@ -2,22 +2,25 @@ from jinja2 import Environment, PackageLoader
 import markdown
 import codecs
 
+# set up jinja and get template to render
 env = Environment(loader=PackageLoader('test', 'templates'))
 template = env.get_template('index.html')
 
-
-#md = markdown.Markdown()
-#post = md.convertFile(input="posts/first-post.markdown", output="static/first-post.html", encoding="utf8")
-#post = md.convert("posts/first-post.markdown")
-
+# generate html from markdown
 input_file = codecs.open("posts/first-post.markdown", mode="r", encoding="utf-8")
 text = input_file.read()
-html = markdown.markdown(text)
 
-static_page_content = template.render(the='variables', go='here', blog=html)
+md = markdown.Markdown(extensions=['meta'])
 
+html = md.convert(text)
+
+# render template context with markdown and other variables
+static_page_content = template.render(the='variables', go='here', blog=html, meta=md.Meta)
+
+# print for my own benefit in the console
 print static_page_content
 
+# write to the new file
 file = open('static/index.html', 'w')
 file.write(static_page_content)
 file.close()
