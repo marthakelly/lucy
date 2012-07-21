08bd9580a1513a5dbad77991eed23428d5f59845
Martha Kelly <mk.girdler@gmail.com>
2012-07-21 12:48:02 -0400

    changing structure to be more like a real python package

--- config.py
+++
@@ -1,9 +0,0 @@
-###### config options ###
-
-config = {
-    "title": "This is a blog, thank you",
-    "author": "Martha Kelly",
-    "template_directory": "templates",
-    "default_page_template": "templates/page-template.html",
-    "build_directory": "build"
-}   
--- lucy/posts/this-is-a-new-post.markdown
+++
@@ -1,5 +0,0 @@
-layout: post
-title: 'this is a new post'
-date: 2012-05-21 18:30
-comments: true
-categories: []
