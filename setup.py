8c0caaf7e61b17cc255495f1b8334d3eb601e8d4
Martha Kelly <mk.girdler@gmail.com>
2013-01-12 13:46:41 -0600

    ermegherd I left this in a terrible state last time. this is clean up

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
 import os
-from setuptools import setup, find_packages
-
-# def read(fname):
-#    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+from setuptools import setup
 
 setup(
     name = "lucy",
     version = "0.0.1",
     author = "Martha Kelly",
     author_email = "mk.girdler@gmail.com",
-    description = ("Pretty & simple static site generator."),
+    description = ("Pretty, simple static site generator."),
     license = "BSD",
     keywords = "lucy static site generator jinja2 markdown blog responsive",
     url = "https://www.github.com/marthakelly/lucy",
@@ -19,7 +16,8 @@ setup(
         'minify'
     ],
     packages = find_packages(),
-    #long_description=read('README.md'),
+    # TODO
+    # long_description=read('README.md'),
     classifiers = [
         "Development Status :: 2 - Pre-Alpha",
         "License :: OSI Approved :: BSD License",
